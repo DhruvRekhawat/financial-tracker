e37921165c2aa2fb8723802d1ffa70c20aedb1ad
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-10-11 22:16:17 +0530

    change import path

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/molecules/navbar'
+import Navbar from '../components/molecules/navbar'
 import { auth } from '@/firebase/firebase'
 import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
